d9bb385029fcc2cc86eca8b7b7638e4aea590ce6
gourav0919 <gouravkhurana919@gmail.com>
2022-04-01 12:16:45 +0530

    Understanding the nginx and add port let's see if it works out or not

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const server = require("http").createServer(app);
 
 const io = require("socket.io")(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: process.env.FRONT_URL,
     methods: ["GET", "POST"],
   },
 });
